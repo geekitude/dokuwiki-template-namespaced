8cf6820d45d694d6ea0186c4189f4f524dcd4e3b
Simon DELAGE <sdelage@gmail.com>
2021-10-15 09:57:56 +0200

    Improved `settings.php`
    Better highlighting of default values

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -9,4 +9,5 @@
  * English language file for Namespaced template config
 */
 
-$lang['combonav']   = 'Combine navigation buttons or not. Priority order will be: back to article > namespace home > parent namespace > wiki home. **[default: enabled]**';
+$lang['combonav']       = 'Combine navigation buttons or not. Priority order will be: back to article > namespace home > parent namespace > wiki home. [default: <code>enabled</code>]';
+$lang['nsindexexclude'] = 'Coma separated list of pages to exclude from navbar current namespace index. [default: <code>sidebar,topbar</code>]';
